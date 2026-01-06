d37598ea09098df2715d51d6eaca1b336a89d2e5
Er-MayankRajput <mnkrjpt408@gmail.com>
2026-01-07 00:35:23 +0530

    6. adding product model for new product creation

--- backend/controlles/productController.js
+++ backend/controlles/productController.js
@@ -1,3 +1,27 @@
-exports.getProducts = (req, res, next) => {
-  res.status(200).json({ message: "Get all products" });
+const Product = require("../models/product");
+
+//creating new product => /api/v1/products/new
+
+exports.newProduct = async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//Controller to get all products => /api/v1/products
+
+exports.getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getProducts } = require("../controlles/productController");
+const { getProducts, newProduct } = require("../controlles/productController");
 
+//Route to get all products
 router.route("/products").get(getProducts);
+router.route("/products/new").post(newProduct);
 
 module.exports = router;
